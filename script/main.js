07aab692913851218ac45ccb5ddbadc5ade8f853
Sizon <sizon@vidrada.org>
2020-08-06 17:12:18 +0300

    add main section

---
+++ script/main.js
@@ -0,0 +1,10 @@
+// console.log("123");
+
+let instance = new vidbg('.video', {
+  mp4: "video/world.mp4",
+  webm: "video/world.webm",
+  poster: "img/poster.jpg",
+  overlay: false,
+});
+
+let rellax = new Rellax('.rocket');
